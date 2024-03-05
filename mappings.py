dc0a84b18e3ea1097b5f7297b150ff2b17845778
radhika-acc <68595824+radhika-acc@users.noreply.github.com>
2024-03-05 14:33:23 -0500

    Add files via upload

---
+++ mappings.py
@@ -0,0 +1,33 @@
+client_revenue_mapping = {
+    '100K or less': '0 - 100K',
+    '100K to 250K': '100K - 250K',
+    '250K to 500K': '250K - 500K',
+    '500K to 1M': '500K - 1000K',
+    'More than 1M': '1000K'
+}
+
+client_employee_mapping = {
+    '1K or less': '1K',
+    '1K to 5K': '1K - 5K',
+    '5K to 15K': '5K - 15K',
+    '15K to 25K': '15K - 25K',
+    'More than 25K': '25K',
+}
+
+business_last_year_mapping = {
+    '0 (No business)': '0',
+    '0 - 25,000': '0 - 25K',
+    '25,000 - 50,000': '25K - 50K',
+    '50,000 - 100,000': '50K - 100K',
+    'More than 100,000': '100K'
+}
+
+opportunity_sizing_mapping = {
+    '10K or less': '10K',
+    '10K to 20K': '10K - 20K',
+    '20K to 30K': '20K - 30K',
+    '30K to 40K': '30K - 40K',
+    '40K to 50K': '40K - 50K',
+    '50K to 60K': '50K - 60K',
+    'More than 60K': '60K'
+}
